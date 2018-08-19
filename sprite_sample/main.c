1bd6696f2a3c92c7e3dbdcc5a492cff4e004648f
huiyu <gmldb@naver.com>
2018-08-19 13:04:36 +0900

    Modify sprite_animation sample
    Signed-off-by: huiyu <gmldb@naver.com>

--- sprite_sample/main.c
+++ sprite_sample/main.c
@@ -16,33 +16,33 @@ main(int argc, char **argv)
     if (0 != SDL_Init(SDL_INIT_EVERYTHING)) {
        return -1;
     }
-	ad.window = SDL_CreateWindow(NULL, 600, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-	if (ad.window == 0) {
-			SDL_Quit();
-			return -1;
-	}
+    ad.window = SDL_CreateWindow(NULL, 2000, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    if (ad.window == 0) {
+        SDL_Quit();
+        return -1;
+    }
     ad.player_surface = SDL_LoadBMP("./test.bmp");
     SDL_SetColorKey(ad.player_surface, 1, SDL_MapRGB(ad.player_surface->format, 255, 0, 255));
     ad.window_surface = SDL_GetWindowSurface(ad.window);
 
     SDL_bool isQuit = SDL_FALSE;
 
-	SDL_Rect rcSprite = {0, 0, 50, 50};
-	SDL_Rect dstSprite = {0, 0};
+    SDL_Rect rcSprite = {0, 0, 50, 50};
+    SDL_Rect dstSprite = {0, 0};
     int player_idx = 0;
     int player_state = 0;
 	
     int lastTime = SDL_GetTicks();
-	int fpsLastTime = SDL_GetTicks();
+    int fpsLastTime = SDL_GetTicks();
     int frameCount = 0;
-    const int FPS = 60;
+    const int FPS = 20;
     while(!isQuit) {
-	    SDL_Event event;
+        SDL_Event event;
         if(SDL_PollEvent(&event)) {
             switch(event.type)
             {
             case SDL_QUIT:
-            isQuit = SDL_TRUE;
+                isQuit = SDL_TRUE;
             break;
             case SDL_KEYUP:
              switch(event.key.keysym.scancode)
@@ -78,10 +78,13 @@ main(int argc, char **argv)
             if(player_idx  >= 5)
                 player_idx  = 0;
         
+
             rcSprite.x = 50 * player_idx;
             rcSprite.y = 50 * player_state;
-            SDL_FillRect(ad.window_surface, NULL, SDL_MapRGB(ad.window_surface->format, 0, 0, 0));
-            SDL_BlitSurface(ad.player_surface,	&rcSprite, ad.window_surface, &dstSprite);
+
+            SDL_FillRect(ad.window_surface, NULL, SDL_MapRGB(ad.window_surface->format, 230, 210, 60));
+/           SDL_BlitSurface(ad.player_surface,	&rcSprite, ad.window_surface, &dstSprite);
+//            SDL_BlitSurface(ad.player_surface, &rcSprite, ad.window_surface, NULL);
         
             SDL_UpdateWindowSurface(ad.window);
             lastTime = SDL_GetTicks();
